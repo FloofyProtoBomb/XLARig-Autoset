d3e8925429afed2f54bafc837fbfda496e17cef7
FloofyProtoBomb <96898033+FloofyProtoBomb@users.noreply.github.com>
2024-02-14 23:26:11 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-./xlarig --donate-level 1 -o us.scala.herominers.com:1190 -u SvjW8dTVzxR4XKjqAneptFQx3j7s4kuGhQ9YwSjPipkLUv8nptyTtrtemud8si98sM2T8mNvdRzrN9KV1nmVz86B2amjCmtN7 -p SAM_A11_01 -a panthera -k
+./xlarig --donate-level 1 -o 141.95.126.31:1190 -u SvjW8dTVzxR4XKjqAneptFQx3j7s4kuGhQ9YwSjPipkLUv8nptyTtrtemud8si98sM2T8mNvdRzrN9KV1nmVz86B2amjCmtN7 -p SAM_A11_01 -a panthera -k
