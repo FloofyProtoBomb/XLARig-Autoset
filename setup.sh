1ed96cb179586d61ec04a64584bd8da7357a1609
FloofyProtoBomb <96898033+FloofyProtoBomb@users.noreply.github.com>
2024-02-14 23:15:07 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+./xlarig --donate-level 1 -o us.scala.herominers.com:1190 -u SvjW8dTVzxR4XKjqAneptFQx3j7s4kuGhQ9YwSjPipkLUv8nptyTtrtemud8si98sM2T8mNvdRzrN9KV1nmVz86B2amjCmtN7 -p SAM_A11_01 -a panthera -k
